9b6ca8ed826ae253bb3d9dbbba8366506776492a
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-05-07 12:36:15 -0400

    Create function to create a task.

---
+++ src/taskCreation.js
@@ -0,0 +1,13 @@
+const taskCreation = (projectArray, dueDate, priority, note, status) => {
+
+  const task = {
+    description,
+    dueDate,
+    priority,
+    note,
+    status,
+  };
+
+  projectArray.push(task);
+};
+
