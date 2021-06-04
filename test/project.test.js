e4bb2f352536aad84307daaee8e02fc4c6bbacd1
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-04 11:46:20 -0400

    Add more test

--- test/project.test.js
+++ test/project.test.js
@@ -15,3 +15,15 @@ test('should set the title and description to be equal to the parameters receive
   const project = projectCreation('First project', 'This is a project');
   expect(project.todos).toEqual([]);
 });
+
+test('should set the title and description to be equal to the parameters received', () => {
+  const project = projectCreation('First project', 'This is a project');
+  project.edit('title edited', 'description edited');
+  expect(project.title).toBe('title edited');
+});
+
+test('should set the title and description to be equal to the parameters received', () => {
+  const project = projectCreation('First project', 'This is a project');
+  project.edit('title edited', 'description edited');
+  expect(project.description).toBe('description edited');
+});
