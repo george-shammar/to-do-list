655b34626dbb440fd8218bcefa50566279d7a624
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-06-04 12:00:26 -0400

    Modify the sentences

--- test/project.test.js
+++ test/project.test.js
@@ -1,28 +1,28 @@
 const projectCreation = require('../project');
 
 
-test('should set the title and description to be equal to the parameters received', () => {
+test('should set the title to be equal to the first parameter received', () => {
   const project = projectCreation('First project', 'This is a project');
   expect(project.title).toBe('First project');
 });
 
-test('should set the title and description to be equal to the parameters received', () => {
+test('should set the description to be equal to the  second parameter received', () => {
   const project = projectCreation('First project', 'This is a project');
   expect(project.description).toBe('This is a project');
 });
 
-test('should set the title and description to be equal to the parameters received', () => {
+test('should return an empty todos array', () => {
   const project = projectCreation('First project', 'This is a project');
   expect(project.todos).toEqual([]);
 });
 
-test('should set the title and description to be equal to the parameters received', () => {
+test('should set the title to the first parameter passed to the edit function', () => {
   const project = projectCreation('First project', 'This is a project');
   project.edit('title edited', 'description edited');
   expect(project.title).toBe('title edited');
 });
 
-test('should set the title and description to be equal to the parameters received', () => {
+test('should set the title to the first parameter passed to the edit function', () => {
   const project = projectCreation('First project', 'This is a project');
   project.edit('title edited', 'description edited');
   expect(project.description).toBe('description edited');
