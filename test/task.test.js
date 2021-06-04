ce9a7c26af9792e314ef14e22735c2ff8e1ef33f
george-shammar <george.gbenle@outlook.com>
2021-06-04 20:35:37 +0000

    Refactor code

--- test/task.test.js
+++ test/task.test.js
@@ -12,44 +12,38 @@ const projectCreation = (title, description) => ({
 
 
 const newProject = projectCreation('title one', 'title description');
-
+const task = taskCreation('task description', '12/03/2021', 'high', newProject);
+ 
 test('Should set the description to be equal to the first parameter received', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].description).toBe('task description');    
 });
 
 test('The description should not be null', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].description).not.toBe(null);    
 });
 
 test('Should set the dueDate to be equal to the second parameter received', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].dueDate).toBe('12/03/2021');    
 });
 
 test('The dueDate should not be null.', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].dueDate).not.toBe(null);    
 });
 
 test('Should set the priority to be equal to the third parameter received', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].priority).toBe('high');    
 });
 
 test('Priority shoud not be empty', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].priority).not.toBe(null);    
 });
 
 test('task should belong to a project', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
   expect(newProject.todos[0].project).not.toBe(null);    
 });
 
 test('Should edit the task description', () => {
-    const task = taskCreation('task description', '12/03/2021', 'high', newProject);
     newProject.todos[0].edit('edited description', '12/03/2021', 'high');
     expect(newProject.todos[0].description).toBe('edited description');    
 });
+
